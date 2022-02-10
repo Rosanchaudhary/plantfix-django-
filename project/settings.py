77b0bfd1b510436f3c60452e5d91eaf9f7bfba66
Roshan <chaudharyroshan2020@gmail.com>
2022-02-10 19:03:08 +0545

    Initial commit

--- project/settings.py
+++ project/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # import cloudinary
 # import cloudinary.uploader
 # import cloudinary.api
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 
 from pathlib import Path
 import os
